08d2361102e63fc020fea06ccd881ba9029852a2
simonkcoding <ykwok3@uncc.edu>
2018-11-22 15:39:23 -0500

    123

--- client/src/components/pages/Grades/Grades.js
+++ client/src/components/pages/Grades/Grades.js
@@ -10,28 +10,82 @@ class Grades extends Component {
     english: [],
     math: [],
     biology: [],
-    geography: []
+    geography: [],
+    grade,
+    newGradebook: []
   };
 
   componentDidMount() {
     this.gradeFilter();
-    console.log(grade);
+    this.initializeGradebook()
+  }
+
+  initializeGradebook = () => {
+    this.groupCoursebook(grade, 'courseName')
+  }
+
+  groupCoursebook = (GradeFromJson, groupProperty) => {
+    GradeFromJson.reduce((reconstructedGradebook, assignment) => {
+      let key = assignment[groupProperty];
+      if (!reconstructedGradebook[key]) {
+        reconstructedGradebook[key] = [];
+      }
+      reconstructedGradebook[key].push(assignment);
+      return reconstructedGradebook;
+    })
+    // if anyone think of a better way to reduce the grade array
+    // feel free to make change
+    delete GradeFromJson[0].id;
+    delete GradeFromJson[0].courseName;
+    delete GradeFromJson[0].assignmentName;
+    delete GradeFromJson[0].subject;
+    delete GradeFromJson[0].date;
+    delete GradeFromJson[0].score;
+    delete GradeFromJson[0].category;
+
+    // this.setState({ newGradebook: GradeFromJson[0] })
+
+    let allHomework = Object.values(GradeFromJson[0])
+
+    let avgScoreArray = [];
+    for (var i = 0; i < allHomework.length; i++) {
+      let sum = allHomework[i].reduce(function (sum, assignment) {
+        return sum + assignment.score;
+      }, 0);
+      const average = Math.round(sum / allHomework[i].length);
+      avgScoreArray.push(average)
+
+      this.setState({ avg: avgScoreArray })
+    }
+
+    console.log('this is :' + JSON.stringify(allHomework))
+    console.log(avgScoreArray)
+
+    let refinedGrade = allHomework.slice()
+
+    for (var i = 0; i<refinedGrade.length;i++){
+      refinedGrade[i].unshift({average:avgScoreArray[i]})
+    }
+
+
+    console.log('this is the last:' + JSON.stringify(refinedGrade))
+    this.setState({ newGradebook: refinedGrade })
   }
 
   gradeFilter = () => {
     this.setState({
       english: grade.filter(course => course.courseName === "English 3")
-    },()=>(console.log('Eng: '+this.state.english)));
+    }, () => (console.log(JSON.stringify(this.state.newGradebook))));
     this.setState({
       math: grade.filter(course => course.courseName === "Math 3")
-    },()=>(console.log('math: '+this.state.math)));
+    }, () => (console.log('math: ' + this.state.math)));
 
     this.setState({
       geography: grade.filter(course => course.courseName === "Geography")
-    },()=>(console.log('geo: '+this.state.geography)));
+    }, () => (console.log('geo: ' + this.state.geography)));
     this.setState({
       biology: grade.filter(course => course.courseName === "Biology")
-    },()=>(console.log('nbio: '+this.state.biology)));
+    }, () => (console.log('nbio: ' + this.state.biology)));
   };
 
   calculateLetterGrade = numGrade => {
@@ -44,8 +98,8 @@ class Grades extends Component {
     return letterGrade;
   };
 
-  handleOnClick=(e)=>{
-    
+  handleOnClick = (e) => {
+
   }
 
   render() {
@@ -78,135 +132,57 @@ class Grades extends Component {
                 </div>
               </div>
             </div>
-            <div className="card grades-cards">
-              <div className="card-content">
-                <div className="holder score-holder">
-                  <div className="columns">
-                    <div className="column is-4">
-                      <p className="class-title">
-                        {this.state.english.length &&
-                          this.state.english[0].courseName}
-                      </p>
-                    </div>
-                    <div className="column is-4">
-                      <p className="class-score">
-                        {this.state.english.length &&
-                          this.state.english[0].score}{" "}
-                        -{" "}
-                        {this.state.english.length &&
-                          this.calculateLetterGrade(
-                            this.state.english[0].score
-                          )}
-                      </p>
-                    </div>
-                    <div className="column is-4">
-                      <p className="view-assignments">
-                        <span 
-                        className="button view-assignments-button"
-                        name='English'
-                        onClick={this.handleOnClick}
-                        >
-                          View Assignments
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="card grades-cards">
-              <div className="card-content">
-                <div className="columns">
-                  <div className="column is-4">
-                    <p className="class-title">
-                      {this.state.math.length && this.state.math[0].courseName}
-                    </p>
-                  </div>
-                  <div className="column is-4">
-                    <p className="class-score">
-                      {this.state.math.length && this.state.math[0].score} -{" "}
-                      {this.state.math.length &&
-                        this.calculateLetterGrade(this.state.math[0].score)}
-                    </p>
-                  </div>
-                  <div className="column is-4">
-                    <p className="view-assignments">
-                      <span className="button view-assignments-button">
-                        View Assignments
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="card grades-cards">
-              <div className="card-content">
-                <div className="columns">
-                  <div className="column is-4">
-                    <p className="class-title">
-                      {this.state.biology.length &&
-                        this.state.biology[0].courseName}
-                    </p>
-                  </div>
+            {/* grade-card */}
 
-                  <div className="column is-4">
-                    <p className="class-score">
-                      {this.state.biology.length && this.state.biology[0].score}{" "}
-                      -{" "}
-                      {this.state.biology.length &&
-                        this.calculateLetterGrade(this.state.biology[0].score)}
-                    </p>
-                  </div>
-                  <div className="column is-4">
-                    <p className="view-assignments">
-                      <span className="button view-assignments-button">
-                        View Assignments
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="card grades-cards">
-              <div className="card-content">
-                <div className="columns">
-                  <div className="column is-4">
-                    <p className="class-title">
-                      {this.state.geography.length &&
-                        this.state.geography[0].courseName}
-                    </p>
-                  </div>
+            {Object.values(this.state.newGradebook).map(subject => (
+              <div className="card grades-cards">
+                <div className="card-content">
+                  <div className="holder score-holder">
+                    <div className="columns">
+                      <div className="column is-4">
+                        <p className="class-title">
+                          {/* get the subject name */}
+                          {subject[1].courseName}
 
-                  <div className="column is-4">
-                    <p className="class-score">
-                      {this.state.geography.length &&
-                        this.state.geography[0].score}{" "}
-                      -{" "}
-                      {this.state.geography.length &&
-                        this.calculateLetterGrade(
-                          this.state.geography[0].score
-                        )}
-                    </p>
-                  </div>
-                  <div className="column is-4">
-                    <p className="view-assignments">
-                      <span className="button view-assignments-button">
-                        View Assignments
-                      </span>
-                    </p>
+                        </p>
+                      </div>
+                      <div className="column is-4">
+                        <p className="class-score">
+                          {subject[0].average}{" "}
+                          -{" "}
+                          {this.state.english.length &&
+                            this.calculateLetterGrade(
+                              subject[0].average
+                            )}
+                        </p>
+                      </div>
+                      <div className="column is-4">
+                        <p className="view-assignments">
+                          <span
+                            className="button view-assignments-button"
+                            name={subject[0].courseName}
+                            onClick={this.handleOnClick}
+                          >
+                            View Assignments
+                         </span>
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
+            {/* end of grade card */}
           </div>
         ) : (
-          <div>
-            {console.log(loggedIn)}
-            <Redirect to={{ pathname: this.state.redirectTo }} />
-          </div>
-        )}
+            <div>
+              {console.log(loggedIn)}
+              <Redirect to={{ pathname: this.state.redirectTo }} />
+            </div>
+          )}
       </div>
     );
   }
 }
 export default Grades;
+
